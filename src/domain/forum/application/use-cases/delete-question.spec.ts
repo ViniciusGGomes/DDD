5edcca6f67a702ab17d7e7c054e230659bfeab78
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-11-10 22:22:30 +0000

    feat(application): add delete question use case
    Implements the 'DeleteQuestionUseCase', responsible for allowing an author to delete their own question.
    
    This use case validates two main rules:
    1. It checks if the question exists.
    2. It verifies that the 'authorId' making the request is the same as the 'authorId' on the question (authorization).
    
    If both checks pass, the question is deleted from the repository.
    
    Unit tests are included to cover the successful deletion scenario and the authorization flow, ensuring a user cannot delete another user's question.

---
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,51 @@
+import { InMemoryQuestionsRepository } from "@/test/repositories/in-memory-questions-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { DeleteQuestionUseCase } from "./delete-question";
+import { makeQuestion } from "@/test/factory/make-question";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: DeleteQuestionUseCase;
+
+describe("Delete Question", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository);
+  });
+
+  it("should be able to delete a question", async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID("author-1"),
+      },
+      new UniqueEntityID("question-1")
+    );
+
+    // console.log(createQuestion);
+
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    await sut.execute({
+      authorId: "author-1",
+      questionId: "question-1",
+    });
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0);
+  });
+
+  it("should not be able to delete a question from another user", async () => {
+    const createQuestion = makeQuestion(
+      { authorId: new UniqueEntityID("author-1") },
+      new UniqueEntityID("question-2")
+    );
+
+    await inMemoryQuestionsRepository.create(createQuestion);
+
+    await expect(() => {
+      return sut.execute({
+        authorId: "author-2",
+        questionId: "question-2",
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+});
