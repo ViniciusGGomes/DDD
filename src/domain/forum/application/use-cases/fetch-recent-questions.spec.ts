444d4305fdd7441c9ad2020170c2aedb2ce26fea
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-11-10 22:39:03 +0000

    feat(application): add fetch recent questions use case
    Implements the 'FetchRecentQuestionsUseCase', which is responsible for retrieving the most recent questions from the forum.
    
    This use case leverages the  method from the  and handles pagination by accepting a 'page' parameter.
    
    Unit tests are included to validate two key scenarios:
    1. The correct ordering of questions, ensuring the most recent items are returned first.
    2. The pagination logic, verifying that requesting a specific page (e.g., page 2) returns the correct subset of items from a larger collection.

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -0,0 +1,48 @@
+import { InMemoryQuestionsRepository } from "@/test/repositories/in-memory-questions-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { FetchRecentQuestionsUseCase } from "./fetch-recent-questions";
+import { makeQuestion } from "@/test/factory/make-question";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: FetchRecentQuestionsUseCase;
+
+describe("Fetch recent questions", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new FetchRecentQuestionsUseCase(inMemoryQuestionsRepository);
+  });
+
+  it("should be able to fetch recent questions", async () => {
+    await inMemoryQuestionsRepository.create(
+      makeQuestion({ createdAt: new Date("2025-0-10") })
+    );
+    await inMemoryQuestionsRepository.create(
+      makeQuestion({ createdAt: new Date("2025-0-11") })
+    );
+    await inMemoryQuestionsRepository.create(
+      makeQuestion({ createdAt: new Date("2025-0-12") })
+    );
+
+    const { questions } = await sut.execute({
+      page: 1,
+    });
+
+    expect(questions).toEqual([
+      expect.objectContaining({ createdAt: new Date("2025-0-12") }),
+      expect.objectContaining({ createdAt: new Date("2025-0-11") }),
+      expect.objectContaining({ createdAt: new Date("2025-0-10") }),
+    ]);
+  });
+
+  it("should be able to fetch paginated recent questions", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionsRepository.create(makeQuestion());
+    }
+
+    const { questions } = await sut.execute({
+      page: 2,
+    });
+
+    expect(questions).toHaveLength(2);
+  });
+});
